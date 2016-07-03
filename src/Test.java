8d9bb407759764885c0252d5fab4d314dfc90027
andreas01060 <andreas.bally@gmail.com>
2016-07-03 18:45:02 +0200

    ik heb de message gedit

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,9 @@
  */
 public class Test {
 
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         System.out.println("NIGGA");
+        System.out.println("In Da Hood");
     }
 }

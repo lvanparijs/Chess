0ff9f13dff1a25f9c96a6c386b73df3fa7a92c10
lvanparijs <lvanparijs@gmail.com>
2016-07-05 18:06:56 +0200

    NullPointer Fixed + printBoard() update

--- src/Essentials/Board.java
+++ src/Essentials/Board.java
@@ -33,58 +33,58 @@ public class Board {
 
 
     public void printBoard(){
-        for (int i=0; i<8;i++){
-            for (int j=0; j<8;j++){
+        for (int i=0; i<spaces.length;i++){
+            for (int j=0; j<spaces.length;j++){
                 //String x = Board.getInstance().spaces[j][i].getType();
 
-                if (Board.getInstance().spaces[j][i].getType()!=null){
-                    System.out.printf("%s",Board.getInstance().spaces[j][i].getType());
+                if (spaces[j][i]!=null){
+                    System.out.print(spaces[j][i].getType().charAt(0)+" ");
                 }
                 else {
-                    System.out.printf("%10%s","Nothing");
+                    System.out.print("- ");
                 }
 
             }
-            System.out.printf("%n");
+            System.out.println();
         }
     }
 
 
 
     public void init(){
-        Board.getInstance().spaces[0][0] = new Rook(0,0,Color.black);
-        Board.getInstance().spaces[1][0] = new Knight(1,0,Color.black);
-        Board.getInstance().spaces[2][0] = new Bishop(2,0,Color.black);
-        Board.getInstance().spaces[3][0] = new Queen(3,0,Color.black);
-        Board.getInstance().spaces[4][0] = new King(4,0,Color.black);
-        Board.getInstance().spaces[5][0] = new Bishop(5,0,Color.black);
-        Board.getInstance().spaces[6][0] = new Knight(6,0,Color.black);
-        Board.getInstance().spaces[7][0] = new Rook(7,0,Color.black);
-        Board.getInstance().spaces[0][1] = new Pawn(0,1,Color.black);
-        Board.getInstance().spaces[1][1] = new Pawn(1,1,Color.black);
-        Board.getInstance().spaces[2][1] = new Pawn(2,1,Color.black);
-        Board.getInstance().spaces[3][1] = new Pawn(3,1,Color.black);
-        Board.getInstance().spaces[4][1] = new Pawn(4,1,Color.black);
-        Board.getInstance().spaces[5][1] = new Pawn(5,1,Color.black);
-        Board.getInstance().spaces[6][1] = new Pawn(6,1,Color.black);
-        Board.getInstance().spaces[7][1] = new Pawn(7,1,Color.black);
+        spaces[0][0] = new Rook(0,0,Color.black);
+        spaces[1][0] = new Knight(1,0,Color.black);
+        spaces[2][0] = new Bishop(2,0,Color.black);
+        spaces[3][0] = new Queen(3,0,Color.black);
+        spaces[4][0] = new King(4,0,Color.black);
+        spaces[5][0] = new Bishop(5,0,Color.black);
+        spaces[6][0] = new Knight(6,0,Color.black);
+        spaces[7][0] = new Rook(7,0,Color.black);
+        spaces[0][1] = new Pawn(0,1,Color.black);
+        spaces[1][1] = new Pawn(1,1,Color.black);
+        spaces[2][1] = new Pawn(2,1,Color.black);
+        spaces[3][1] = new Pawn(3,1,Color.black);
+        spaces[4][1] = new Pawn(4,1,Color.black);
+        spaces[5][1] = new Pawn(5,1,Color.black);
+        spaces[6][1] = new Pawn(6,1,Color.black);
+        spaces[7][1] = new Pawn(7,1,Color.black);
 
-        Board.getInstance().spaces[0][7] = new Rook(0,7,Color.white);
-        Board.getInstance().spaces[1][7] = new Knight(1,7,Color.white);
-        Board.getInstance().spaces[2][7] = new Bishop(2,7,Color.white);
-        Board.getInstance().spaces[3][7] = new Queen(3,7,Color.white);
-        Board.getInstance().spaces[4][7] = new King(4,7,Color.white);
-        Board.getInstance().spaces[5][7] = new Bishop(5,7,Color.white);
-        Board.getInstance().spaces[6][7] = new Knight(6,7,Color.white);
-        Board.getInstance().spaces[7][7] = new Rook(7,7,Color.white);
-        Board.getInstance().spaces[0][6] = new Pawn(0,6,Color.white);
-        Board.getInstance().spaces[1][6] = new Pawn(1,6,Color.white);
-        Board.getInstance().spaces[2][6] = new Pawn(2,6,Color.white);
-        Board.getInstance().spaces[3][6] = new Pawn(3,6,Color.white);
-        Board.getInstance().spaces[4][6] = new Pawn(4,6,Color.white);
-        Board.getInstance().spaces[5][6] = new Pawn(5,6,Color.white);
-        Board.getInstance().spaces[6][6] = new Pawn(6,6,Color.white);
-        Board.getInstance().spaces[7][6] = new Pawn(7,6,Color.white);
+        spaces[0][7] = new Rook(0,7,Color.white);
+        spaces[1][7] = new Knight(1,7,Color.white);
+        spaces[2][7] = new Bishop(2,7,Color.white);
+        spaces[3][7] = new Queen(3,7,Color.white);
+        spaces[4][7] = new King(4,7,Color.white);
+        spaces[5][7] = new Bishop(5,7,Color.white);
+        spaces[6][7] = new Knight(6,7,Color.white);
+        spaces[7][7] = new Rook(7,7,Color.white);
+        spaces[0][6] = new Pawn(0,6,Color.white);
+        spaces[1][6] = new Pawn(1,6,Color.white);
+        spaces[2][6] = new Pawn(2,6,Color.white);
+        spaces[3][6] = new Pawn(3,6,Color.white);
+        spaces[4][6] = new Pawn(4,6,Color.white);
+        spaces[5][6] = new Pawn(5,6,Color.white);
+        spaces[6][6] = new Pawn(6,6,Color.white);
+        spaces[7][6] = new Pawn(7,6,Color.white);
 
     }
 
